b096c32d9ca3b6821a1a4a00a7cf61426ef98898
Folusakin Abiola <afolusakin@gmil.com>
2021-03-09 02:09:45 -0600

    Choosing and labelling

--- app/src/main/java/com/example/picturesque/MainActivity.java
+++ app/src/main/java/com/example/picturesque/MainActivity.java
@@ -1,56 +1,195 @@
 package com.example.picturesque;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.MediaStore;
+import android.media.Image;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.google.mlkit.vision.common.InputImage;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+public class MainActivity extends  AppCompatActivity {
+
+    InputImage image;
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        button = (Button)findViewById(R.id.buttonLoadPicture);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                openGallery();
+
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
         }
 
-        return super.onOptionsItemSelected(item);
+        try {
+
+            image = InputImage.fromFilePath(getApplicationContext(), imageUri);
+            System.out.println("the image: "+image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // [END image_from_path]
+        labelImages(image); // Magic Here
+        labelImages(image); // Magic Here
+
+
+        configureAndRunImageLabeler(image);
+
+
+    }
+
+    public void displayLotteryNumberToUser(String randomNumber) {
+        TextView randomNumberTv = (TextView) findViewById(R.id.textView2);
+        randomNumberTv.setText(randomNumber.toString());
+    }
+
+    public void labelImages(InputImage image) {
+        ImageLabelerOptions options =
+                new ImageLabelerOptions.Builder()
+                        .setConfidenceThreshold(0.8f)
+                        .build();
+
+        // [START get_detector_options]
+        ImageLabeler labeler = ImageLabeling.getClient(options);
+        // [END get_detector_options]
+
+        /*
+        // [START get_detector_default]
+        // Or use the default options:
+        //ImageLabeler detector = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+        // [END get_detector_default]
+        */
+
+        // [START run_detector]
+        Task<List<ImageLabel>> result =
+                labeler.process(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<ImageLabel>>() {
+                                    @Override
+                                    public void onSuccess(List<ImageLabel> labels) {
+                                        // Task completed successfully
+                                        // [START_EXCLUDE]
+                                        // [START get_labels]
+                                        for (ImageLabel label : labels) {
+                                            String text = label.getText();
+                                            float confidence = label.getConfidence();
+                                        }
+                                        // [END get_labels]
+                                        // [END_EXCLUDE]
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+        // [END run_detector]
+    }
+
+    public void configureAndRunImageLabeler(InputImage image) {
+        ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+        labeler.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<ImageLabel> labels) {
+                        // Task completed successfully
+                        // ...
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+
+        labeler.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<ImageLabel> labels) {
+                        // [START get_image_label_info]
+                        List<String> label2 = new ArrayList<String>();
+                        for (ImageLabel label : labels) {
+
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            int index = label.getIndex();
+
+                            label2.add(text);
+                            System.out.println("THESE are the index NUMBERS HHAHAH!!!!!!!: "+text);
+
+                            System.out.println("THESE are the index NUMBERS after HHAHAH!!!!!!!: "+text);
+                            System.out.println(label2);
+
+
+
+                        }
+                        System.out.println("Outside the main: "+ label2);
+                        String dude = " ";
+                        for (int i = 0; i < label2.size(); i++) {
+
+                            if(i>0 && label2.get(i) == "Dude")
+                                dude = label2.get(i);
+                            else if (i>0)
+                                dude = dude+label2.get(i)+"\n";
+
+                        }
+                        System.out.println("Outside the dude main: "+ dude);
+                        displayLotteryNumberToUser(dude);
+                        // [END get_image_label_info]
+                    }
+                });
+
+
     }
 }
