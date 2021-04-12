b8e256bbdf4a0f90b26ac00f5faae11898a88c6f
kscutt <51063862+kscutt@users.noreply.github.com>
2021-04-12 02:29:50 -0500

    OH SNAP -- Listed Files and Got the MD5 HASH

--- app/src/main/java/com/example/picturesque/UploadImg.java
+++ app/src/main/java/com/example/picturesque/UploadImg.java
@@ -21,8 +21,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -32,7 +35,7 @@ import java.util.UUID;
 public class UploadImg extends AppCompatActivity {
 
     // views for button
-    private Button btnSelect, btnUpload;
+    private Button btnSelect, btnUpload, btnBack;
 
     // view for image view
     private ImageView imageView;
@@ -47,8 +50,7 @@ public class UploadImg extends AppCompatActivity {
     FirebaseStorage storage;
     StorageReference storageReference;
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload_img);
 
@@ -63,6 +65,7 @@ public class UploadImg extends AppCompatActivity {
         btnSelect = findViewById(R.id.btnChoose);
         btnUpload = findViewById(R.id.btnUpload);
         imageView = findViewById(R.id.imageView);
+        btnBack = findViewById(R.id.btnBack);
 
         // get the Firebase  storage reference
         storage = FirebaseStorage.getInstance();
@@ -71,8 +74,7 @@ public class UploadImg extends AppCompatActivity {
         // on pressing btnSelect SelectImage() is called
         btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 SelectImage();
             }
         });
@@ -80,15 +82,14 @@ public class UploadImg extends AppCompatActivity {
         // on pressing btnUpload uploadImage() is called
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 uploadImage();
             }
         });
     }
+
     // Select Image method
-    private void SelectImage()
-    {
+    private void SelectImage() {
 
         // Defining Implicit Intent to mobile gallery
         Intent intent = new Intent();
@@ -100,12 +101,12 @@ public class UploadImg extends AppCompatActivity {
                         "Select Image from here..."),
                 PICK_IMAGE_REQUEST);
     }
+
     // Override onActivityResult method
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
-                                    Intent data)
-    {
+                                    Intent data) {
 
         super.onActivityResult(requestCode,
                 resultCode,
@@ -132,9 +133,7 @@ public class UploadImg extends AppCompatActivity {
                                 getContentResolver(),
                                 filePath);
                 imageView.setImageBitmap(bitmap);
-            }
-
-            catch (IOException e) {
+            } catch (IOException e) {
                 // Log the exception
                 e.printStackTrace();
             }
@@ -143,8 +142,7 @@ public class UploadImg extends AppCompatActivity {
     }
 
     // UploadImage method
-    private void uploadImage()
-    {
+    private void uploadImage() {
         if (filePath != null) {
 
             // Code for showing progressDialog while uploading
@@ -160,16 +158,14 @@ public class UploadImg extends AppCompatActivity {
                             "images/"
                                     + UUID.randomUUID().toString());
 
-            // adding listeners on upload
-            // or failure of image
+
             ref.putFile(filePath)
                     .addOnSuccessListener(
                             new OnSuccessListener<UploadTask.TaskSnapshot>() {
 
                                 @Override
                                 public void onSuccess(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
+                                        UploadTask.TaskSnapshot taskSnapshot) {
 
                                     // Image uploaded successfully
                                     // Dismiss dialog
@@ -180,12 +176,16 @@ public class UploadImg extends AppCompatActivity {
                                                     Toast.LENGTH_SHORT)
                                             .show();
                                 }
-                            })
 
+
+                            }
+
+
+                    )
+// This is not the On failure listener we want to get rid of
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
-                        public void onFailure(@NonNull Exception e)
-                        {
+                        public void onFailure(@NonNull Exception e) {
 
                             // Error, Image not uploaded
                             progressDialog.dismiss();
@@ -203,18 +203,92 @@ public class UploadImg extends AppCompatActivity {
                                 // percentage on the dialog box
                                 @Override
                                 public void onProgress(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
+                                        UploadTask.TaskSnapshot taskSnapshot) {
                                     double progress
                                             = (100.0
                                             * taskSnapshot.getBytesTransferred()
                                             / taskSnapshot.getTotalByteCount());
                                     progressDialog.setMessage(
                                             "Uploaded "
-                                                    + (int)progress + "%");
+                                                    + (int) progress + "%");
                                 }
                             });
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+
+            // [START metadata_get_storage_reference]
+            // Create a storage reference from our app
+            StorageReference storageRef = storage.getReference();
+
+            // Get reference to the file
+            StorageReference forestRef = storageRef.child("images/3fc6cbaa-9a6d-488a-bcdc-da7432497e1d");
+            // [END metadata_get_storage_reference]
+//TODO: figure out how to automatically get the filepath from Firebase
+
+            // [START get_file_metadata]
+            forestRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+                @Override
+                public void onSuccess(StorageMetadata storageMetadata) {
+                    System.out.println("This is the MD5 Hash: " + storageMetadata.getMd5Hash() );
+                    System.out.println("This is the size in bytes: " + storageMetadata.getSizeBytes() );
+                    // Metadata now contains the metadata for 'images/forest.jpg'
+ //TODO store MD5 Hash for comparison
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    System.out.println("Some sort of error happened >:( ");
+                    // Uh-oh, an error occurred!
+                }
+            });
+            // [END get_file_metadata]
+        }
+
+        StorageReference listRef = storage.getReference().child("images");
+
+        listRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference prefix : listResult.getPrefixes()) {
+                            // All the prefixes under listRef.
+                            // You may call listAll() recursively on them.
+                            System.out.print("HERE are our Prefixes: " + listResult.toString() + "\n");
+                            listRef.listAll();
+                        }
+
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the items under listRef.
+                            System.out.print("HERE are our Items: " + listResult.getItems().toString()+ "\n");
+                            listRef.listAll();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Uh-oh, an error occurred!
+                    }
+                });
+    }
+
+
+    public void buttonClicked(View view) {
+        if (view.getId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            this.finish();
         }
     }
+
+// go back
+   public void backClicked(View view) {
+        if (view.getId() == R.id.btnBack) {
+            Intent intent = new Intent(getApplicationContext(), ChooseImg.class);
+            startActivity(intent);
+        }
+    };
 }
 
